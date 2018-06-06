e11c8e64266821322ff0e6952224bdaa637a254c
taluccia <38057252+taluccia@users.noreply.github.com>
2018-06-06 16:17:07 -0700

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,14 @@
+var photos1 = [{
+	lat: 53.2688,
+	lng: -125.7294,
+	thumbnail: "/beetlefire/img/165.png",
+	url: "/beetlefire/img/165.png",
+	caption: "Sioux Indians"
+},
+	{
+	lat: 54.2688,
+	lng: -125.7294,
+	thumbnail: "img/165.png",
+	url: "img/165.png",
+	caption: "Sioux Indians"
+}];
---
+++ note.md
@@ -0,0 +1,20 @@
+## meeting notes
+
+### layout
+
+#### logo
+
+gravit design 
+
+font color
+
+## the size of the storymap script panel
+
+```css
+@media (min-width: 768px) {
+    .storymap-story {
+        width: 430px !important;
+    }
+
+}
+```
